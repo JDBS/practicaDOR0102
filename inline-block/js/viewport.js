850d44d5424f9fa4e7e8c47b1b5da9bc920ad78f
Jaime D <jdbsull@gmail.com>
2017-10-08 20:17:52 +0100

    first commit

---
+++ inline-block/js/viewport.js
@@ -0,0 +1,11 @@
+
+function calcula(){
+	window.document.getElementById("ancho").innerHTML= "Ancho: " + window.innerWidth + "px, ";
+	window.document.getElementById("alto").innerHTML= "Alto: " + window.innerHeight + "px";
+}
+
+
+function cerrarCookies(){
+		window.document.getElementsByClassName("pie")[0].style.display="none";
+}
+
